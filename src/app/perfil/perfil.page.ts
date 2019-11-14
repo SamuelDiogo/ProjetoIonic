702a840cda1786bd5adf1c3122178846d536f15e
Marketing Digital - 2019.1 <169104792019.1@senacrjedu.local>
2019-11-14 10:20:49 -0300

    perfil criado e melhorar

---
+++ src/app/perfil/perfil.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-perfil',
+  templateUrl: './perfil.page.html',
+  styleUrls: ['./perfil.page.scss'],
+})
+export class PerfilPage implements OnInit {
+
+  constructor(
+    private authService: AuthService
+  ) { }
+
+  ngOnInit() {
+  }
+  async logout(){
+    try{
+      await this.authService.logout();
+    }catch(error){
+      console.error(error);
+    }
+  }
+}
